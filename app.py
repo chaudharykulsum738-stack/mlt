288da830ff9e546c7c39ad2124a7417a0b77bbff
chaudharykulsum738-stack <chaudharykulsum738@gmail.com>
2026-01-21 16:58:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,9 @@ import streamlit as st
 import pandas as pd
 import random
 
-BASE_DIR = Path(__file__).resolve().parent
 
-# Now this works
-DATA_DIR = pick_writable_dir([os.getenv("MHT_DATA_DIR"), BASE_DIR / "data", BASE_DIR])
-CSV_PATH = DATA_DIR / "entries.csv"
-BACKUP_EXCEL_PATH = DATA_DIR / "entries.xlsx"
+BASE_DIR = Path(__file__).resolve().parent
+DESKTOP = Path(os.path.expanduser("~/Desktop"))
 
 
 def pick_writable_dir(candidate_dirs):
