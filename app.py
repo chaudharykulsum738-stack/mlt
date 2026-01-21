45959683939e8581ff26d1d8843e700f0dc62fa1
chaudharykulsum738-stack <chaudharykulsum738@gmail.com>
2026-01-21 17:00:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -404,10 +404,7 @@ page = st.sidebar.radio(
 
 
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-if "username" not in st.session_state:
-    st.session_state.username = ""
+
 
 if page == "🔐 Login":
     st.subheader("🔐 Login")
